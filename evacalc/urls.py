d432c0ad3f763a4dd3d2fd52af935e6ee25ec2a1
OxidNowin <terror20007@mail.ru>
2021-02-27 18:07:28 +0500

    added grade function

--- evacalc/urls.py
+++ evacalc/urls.py
@@ -3,5 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-	path('', views.test, name="test")    
+    #path('login/', auth_views.LoginView.as_view(), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	path('', views.test, name="test"),    
 ]
--- evacalc/views.py
+++ evacalc/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 from .models import JobEvaluation
 from .forms import PostForm, UnlogicalPostForm
-from .arrays import first_arr, second_arr, third_arr, fourth_arr
+from .arrays import skills_arr, responsibility_arr, probl_solv_arr, check_arr, grade_arr
 
 def test(request):
-	if request.method == "POST" and 'first' in request.POST:
+	if request.method == "POST" and 'logical_post' in request.POST:
 		form = PostForm(request.POST)
 		if form.is_valid():
 
@@ -27,8 +27,9 @@ def test(request):
 
 			value, logical_1 = ski_and_kno(tech_skills, knowledge, soft_skills)
 			value_perc, logical_2 = troubles_sol(around_question, question_diff)
-			eva_value, logical_3 = fool_check(value, value_perc)
+			eva_value, logical_3 = union_skills_and_solving(value, value_perc)
 			eva_resp = responsibility(free_move, nature, impact_importance)
+
 			"""Проверка на ненахождение соответствия"""
 
 			if none_check(value, value_perc, eva_value, eva_resp) == False:
@@ -37,12 +38,16 @@ def test(request):
 
 			"""Проверка на ВОДОПАД"""
 
-			if waterfall(tech_skills, around_question, free_move) == False:
+			if waterfall_check(tech_skills, around_question, free_move) == False:
 				return render(request, "evacalc/index.html", {'form': form,
 					'error_message': 'Не соблюден принцип водопада',})				
 
+			sum_of_values = value + eva_value + eva_resp
+			grade = grade_determine(sum_of_values)
+
+			result_str = f"{value} {int(value_perc*100)}% {eva_value} {eva_resp}  summa:{sum_of_values} grade:{grade}"
+
 			"""Проверка на логичность"""
-			result_str = f"{value} {int(value_perc*100)}% {eva_value} {eva_resp}"
 
 			if (logical_1 and logical_2 and logical_3) == False:
 				return render(request, "evacalc/index.html", {'form': form,
@@ -57,8 +62,10 @@ def test(request):
 					'логично_2': logical_2,
 					'eva_value': eva_value,
 					'логично_3': logical_3,
-					'eva_resp': eva_resp}
-			j = JobEvaluation(
+					'eva_resp': eva_resp,
+					'sum_of_values': sum_of_values,
+					'grade': grade}
+			"""j = JobEvaluation(
 				title = title,
 				user = request.user,
 				short_profile = short_profile,
@@ -75,9 +82,10 @@ def test(request):
 				impact_importance = impact_importance, 
 				eva_resp = eva_resp,
 				sum_of_values = value + eva_value + eva_resp)
-			#j.save()
+			j.save()"""
 			return HttpResponse(jopa.items())
-	elif request.method == "POST" and 'dict' in request.POST:
+
+	elif request.method == "POST" and 'unlogical_post' in request.POST:
 		form = UnlogicalPostForm(request.POST)
 		if form.is_valid():
 			stroka = form.cleaned_data['unlogical_result']
@@ -95,7 +103,7 @@ def ski_and_kno(tech_skills, knowledge, soft_skills):
 	"""
 	logical_1 = True
 	
-	for arr in first_arr:
+	for arr in skills_arr:
 		if str(arr[0]) == tech_skills:
 			if str(arr[1]) == knowledge:
 				if str(arr[2]) == soft_skills:
@@ -105,13 +113,14 @@ def ski_and_kno(tech_skills, knowledge, soft_skills):
 	return None, None
 
 def troubles_sol(around_question, question_diff):
+
 	"""
 	Область решаемых вопросов
 	Сложность вопросов
 	"""
 	logical_2 = True
 
-	for arr in second_arr:
+	for arr in probl_solv_arr:
 		if str(arr[0]) == around_question:
 			if str(arr[1]) == question_diff:
 				if str(arr[3]) == "0":
@@ -119,11 +128,11 @@ def troubles_sol(around_question, question_diff):
 				return float(arr[2]), logical_2
 	return None, None
 
-def fool_check(value, value_perc):
+def union_skills_and_solving(value, value_perc):
 
 	logical_3 = True
 
-	for arr in third_arr:
+	for arr in check_arr:
 		if str(arr[0]) == str(value_perc):
 			if str(arr[1]) == str(value):
 				if str(arr[3]) == "0":
@@ -139,21 +148,27 @@ def responsibility(free_move, nature, impact_importance):
 	Важность воздействия
 	"""
 
-	for arr in fourth_arr:
+	for arr in responsibility_arr:
 		if str(arr[0]) == free_move:
 			if str(arr[1]) == nature:
 				if str(arr[2]) == impact_importance:
 					return int(arr[3])
 	return None
 
+def grade_determine(sum_of_values):
+	for summ in grade_arr:
+		if (sum_of_values<=summ[2]) and (sum_of_values>=summ[1]):
+			return summ[0]
+	return None 
+
 def none_check(value, value_perc, eva_value, eva_resp):
 	if (value == None) or (value_perc == None) or (eva_value == None) or (eva_resp == None):
 		return False
 	return True
 
-def waterfall(tech_skills,around_question, free_move):
-	if ord(tech_skills) <= ord(around_question):
+def waterfall_check(tech_skills,around_question, free_move):
+	if ord(tech_skills) < ord(around_question):
 		return False
-	elif ord(around_question) <= ord(free_move):
+	elif ord(around_question) < ord(free_move):
 		return False
 	return True
